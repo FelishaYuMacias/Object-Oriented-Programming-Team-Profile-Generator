87d479ab129352b0c17b4276ebdf64cc6030425b
Felisha <felisha.j.yu@gmail.com>
2022-10-25 09:09:50 -0700

    added prompts for manager and choice of new employee

--- index.js
+++ index.js
@@ -11,22 +11,78 @@ const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
+// empty array for employee list
+const employees = []
+
 // GIVEN a command-line application that accepts user input
-
-// TODO: WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-
-// TODO: WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-
-// TODO: WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
 
 // TODO: WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            //Manager's Name
+            type: 'input',
+            message: "What is the Manager's name?",
+            name: 'name',
+        },
+        {
+            //Manager's ID
+            type: 'input',
+            message: 'What is their employee id?',
+            name: 'id'
+        },
+        {
+            //Manager's email
+            type: 'input',
+            message: 'What is their email address?',
+            name: 'email'
+        },
+        {
+            //Manager's Office Number
+            type: 'input',
+            message: 'What is their office number?',
+            name: 'officeNumber'
+        }
+    ])
+    .then ((managerInfo) =>{
+        console.log (managerInfo)
+        const managerInfo = {name, id, email,officeNumber}
+   const manager= new Manager(name,id,email,officeNumber) 
+   empoylees.push(manager)
+   addNewEmployee ()
+    })
+};
+
 
 // TODO: WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+function addNewEmployee () {
+    inquirer.prompt([
+        {
+            name: "question",
+            type: "list",
+            messag: "Finish building your team.Do you want to:"
+            choices: ["Add an Engineer", "Add an Intern", "My team is complete!"]
+        }
+    ]).then(answers => {
+        switch (answers.question) {
+            case "Add an Engineer":
+                console.log("add Engineer!")
+                addEngineer();
+                break;
+
+            case "Add an Intern":
+                console.log("add Intern!")
+                addIntern();
+                break;
+
+            default:
+                console.log("Your team page is being built")
+                break;
+        }
+    })
+}
 
 // TODO: WHEN I select the engineer option
 // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
@@ -36,3 +92,13 @@ const Intern = require("./lib/Intern")
 
 // TODO: WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
+
+// TODO: WHEN I am prompted for my team members and their information
+managerPrompt ()
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+
+// TODO: WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+
+// TODO: WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
